e5ce7a20e9b633dce4b20d36d7a1d9d00dac5e2b
shoh <behruzrahimov580@gmail.com>
2025-01-27 17:15:43 +0500

    .

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
+
 import Home from '@pages/index';
 
 import { Layout } from '../layout';
 
 export default function App() {
+	useEffect(() => {
+		const bodyClassList = document.body.classList;
+		bodyClassList.add(`theme-dark`);
+	}, []);
+
 	return (
 		<Layout>
 			<Home />
